e99839bcaac302ddc1cd777f85387af6e355ab78
jakob.bratten@gmail.com <jakob.bratten@gmail.com@e8a0255f-7883-1ebb-3e0b-7d8a4db16683>
2012-03-13 14:35:14 +0000

    drawing rays for debug purposes. doesn't work properly yet though

--- trunk/source/CrazeGraphics/Renderer/DrawRays.cpp
+++ trunk/source/CrazeGraphics/Renderer/DrawRays.cpp
@@ -4,12 +4,46 @@
 #include "Graphics.h"
 #include "Buffer/Buffer.h"
 
+#include "DrawRays.h"
+
+#include "EffectUtil/EffectHelper.h"
+#include "EffectUtil/CBufferHelper.hpp"
+
 using namespace Craze;
 using namespace Craze::Graphics2;
 
-void render(std::shared_ptr<UAVBuffer> rays)
+bool DrawRays::initialize()
 {
+	unsigned int args[] = { 0, 1, 0, 0};
+	m_argBuffer = Buffer::CreateArg(gpDevice, sizeof(unsigned int) * 4, args, "DrawRays arg buffer");
 
+	return true;
+}
+
+void DrawRays::render(std::shared_ptr<UAVBuffer> rays, const Matrix4& viewProj)
+{
+	//draw instanced indirect in immediate context (gpdevice->getDeviceContext
+	CBPerObject cbObj;
+	cbObj.world = Matrix4::IDENTITY * viewProj;
+	gpDevice->GetCbuffers()->SetObject(cbObj);
+
+	auto dc = gpDevice->GetDeviceContext();
+
+	//Prepare the argument buffer for the indirect call
+	dc->CopyStructureCount(m_argBuffer->GetBuffer(), 0, rays->GetUAV());
+
+	ID3D11Buffer* vs = nullptr;
+	unsigned int stride = 0;
+	unsigned int offset = 0;
+	dc->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
+	dc->IASetVertexBuffers(0, 1, &vs, &stride, &offset);
+
+	gpDevice->SetRenderTarget(nullptr, nullptr);
+
+	//Draw the rays
+	dc->DrawInstancedIndirect(m_argBuffer->GetBuffer(), 0);
+
+	dc->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 }
 
 
