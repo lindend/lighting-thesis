15ff95fee3e3803df667f7b1828f3abe24a91617
d.linden88@gmail.com <d.linden88@gmail.com@e8a0255f-7883-1ebb-3e0b-7d8a4db16683>
2012-03-14 12:21:29 +0000

    Updated draw rays.

--- source/CrazeGraphics/Renderer/DrawRays.cpp
+++ source/CrazeGraphics/Renderer/DrawRays.cpp
@@ -21,6 +21,10 @@ bool DrawRays::initialize()
 	CD3D11_RASTERIZER_DESC rsDesc(D3D11_FILL_SOLID, D3D11_CULL_NONE, true, 0, 0.f, 0.f, true, false, false, true);
 	gpDevice->GetDevice()->CreateRasterizerState(&rsDesc, &m_rasterizerState);
 
+	CD3D11_DEPTH_STENCIL_DESC dssDesc = CD3D11_DEPTH_STENCIL_DESC(CD3D11_DEFAULT());
+	dssDesc.DepthEnable = false;
+	gpDevice->GetDevice()->CreateDepthStencilState(&dssDesc, &m_dss);
+
 	m_effect.initialize();
 	return true;
 }
@@ -36,6 +40,8 @@ void DrawRays::render(std::shared_ptr<UAVBuffer> rays, const Matrix4& viewProj)
 	auto dc = gpDevice->GetDeviceContext();
 
 	//dc->RSSetState(m_rasterizerState);
+	dc->OMSetDepthStencilState(m_dss, 0);
+
 	//Prepare the argument buffer for the indirect call
 	dc->CopyStructureCount(m_argBuffer->GetBuffer(), 0, rays->GetUAV());
 
@@ -44,16 +50,20 @@ void DrawRays::render(std::shared_ptr<UAVBuffer> rays, const Matrix4& viewProj)
 	unsigned int offset = 0;
 	dc->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_LINELIST);
 	dc->IASetVertexBuffers(0, 1, &vs, &stride, &offset);
-
+	dc->IASetIndexBuffer(nullptr, DXGI_FORMAT_R32_UINT, 0);
+	dc->IASetInputLayout(nullptr);
+	
 	ID3D11ShaderResourceView* srv = rays->GetSRV();
 	gpDevice->GetDeviceContext()->VSSetShaderResources(0, 1, &srv);
 
 	//Draw the rays
-	dc->DrawInstancedIndirect(m_argBuffer->GetBuffer(), 0);
+	dc->Draw(16384, 0);
+	//dc->DrawInstancedIndirect(m_argBuffer->GetBuffer(), 0);
 
 	dc->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	//dc->RSSetState(nullptr);
+	dc->OMSetDepthStencilState(nullptr, 0);
 
 	srv = nullptr;
 	gpDevice->GetDeviceContext()->VSSetShaderResources(0, 1, &srv);
