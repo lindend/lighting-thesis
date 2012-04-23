b7835921868cf1ea4c53590dffe2eece658178de
jakob.bratten@gmail.com <jakob.bratten@gmail.com@e8a0255f-7883-1ebb-3e0b-7d8a4db16683>
2012-04-23 13:13:01 +0000

    added camera values function for benchmarkscript

---
+++ trunk/bin/cameraValues.lua
@@ -0,0 +1,7 @@
+benchmarkCameraData = 
+[
+{px=-669.0f, py=210.0f, pz=38.0f, dx = .7208f, dy = .3397f, dz = .6040f}, 
+{px=-309.0f, py=2333.0f, pz=118.0f, dx = .1667f, dy = -.9859f, dz = .29f}, 
+{px=0.0f, py=0.0f, pz=0.0f, dx = .0f, dy = 1.0f, dz = .0f},
+{px=-922.0f, py=82.0f, pz=185.0f, dx = -.785f, dy = .394f, dz = -.477f},  
+]
